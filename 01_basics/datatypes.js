5d0f87bae770b25599b35b735f534c18d98ff718
Rohan Kumrawat <93284514+rohan-kumrawat@users.noreply.github.com>
2024-05-09 12:36:55 +0000

    dataTypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert() // we are using nodejs, not browser
+
+/* 
+Data Types :
+
+number
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+object
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
